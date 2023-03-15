c292f8f57e48ed5dd84f51f1e38f0a0a2f3697a1
sreenath707 <sreenathsharma707@gmail.com>
2023-03-15 15:18:44 +0530

    login and signup workflows changed a bit

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,36 +1,43 @@
-const users = require("../models/user");
-const jwt = require("jsonwebtoken");
+const users = require('../models/user')
+const jwt = require('jsonwebtoken')
 
-const secret_key = "bussss";
+const secret_key = 'bussss'
 
 function signup(req, res) {
-  const { username, email, password } = req.body;
+  const { username, email, password } = req.body
   users
     .findOne({ username: username })
     .then((user) => {
       if (user) {
-        res.send({ signUpSuccess: false, msg: "account already exists" });
+        res.send({ signUpSuccess: false, msg: 'account already exists' })
       } else {
         users
           .insertMany([req.body])
           .then(() => {
-            console.log("inserted");
-            res.send({ signUpSuccess: true, msg: "inserted" });
+            let token = jwt.sign(
+              { username: username, email: email },
+              secret_key
+            )
+            res.send({
+              signUpSuccess: true,
+              token: token,
+              msg: 'inserted',
+            })
           })
           .catch((err) => {
-            console.error(err);
-            res.send({ signUpSuccess: false, msg: err });
-          });
+            console.error(err)
+            res.send({ signUpSuccess: false, msg: err })
+          })
       }
     })
     .catch((err) => {
-      res.statusCode = 401;
-      res.send({ signUpSuccess: false, msg: err });
-    });
+      res.statusCode = 401
+      res.send({ signUpSuccess: false, msg: err })
+    })
 }
 
 function login(req, res) {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   users
     .findOne({ email: email })
     .then((user) => {
@@ -39,24 +46,24 @@ function login(req, res) {
           let token = jwt.sign(
             { username: user.username, email: user.email },
             secret_key
-          );
+          )
           // res.cookie("authToken", token);
           res.send({
             loggedIn: true,
             user: { username: user.username, email: user.email },
             token: token,
-          });
+          })
         } else {
-          res.send({ loggedIn: false, msg: "incorrect password" });
+          res.send({ loggedIn: false, msg: 'incorrect password' })
         }
       } else {
-        res.send({ loggedIn: false, msg: "user not found" });
+        res.send({ loggedIn: false, msg: 'user not found' })
       }
     })
     .catch((err) => {
-      console.log(err);
-      res.send({ loggedIn: false, msg: err });
-    });
+      console.log(err)
+      res.send({ loggedIn: false, msg: err })
+    })
 }
 
-module.exports = { signup, login };
+module.exports = { signup, login }
